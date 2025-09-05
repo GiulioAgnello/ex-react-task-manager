e13ff04f06253772040e9e6ba8e785815eddef6b
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-09-05 12:08:47 +0200

    Status

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,5 +1,7 @@
-export default function TaskRow({ task }) {
-  // Determina la classe di colore in base allo stato
+import { memo } from "react";
+
+const TaskRow = memo(({ task }) => {
+  // Status e Colore
   let statusBg = "bg-secondary";
   if (task.status === "To do") statusBg = "bg-danger";
   else if (task.status === "Doing") statusBg = "bg-warning text-dark";
@@ -9,7 +11,10 @@ export default function TaskRow({ task }) {
     <li className="list-group-item d-flex justify-content-between align-items-center">
       <span className="fw-bold">{task.title}</span>
       <span className={`badge ${statusBg} mx-2`}>{task.status}</span>
-      <span className="text-muted small">{task.createdAt}</span>
+      <span className="text-muted small">
+        {new Date(task.createdAt).toLocaleDateString()}
+      </span>
     </li>
   );
-}
+});
+export default TaskRow;
