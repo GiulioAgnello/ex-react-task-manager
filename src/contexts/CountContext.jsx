871b58bccb6b3b4b56240881989c2d92500eb67c
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-09-19 15:34:58 +0200

    delete_comment

--- src/contexts/CountContext.jsx
+++ src/contexts/CountContext.jsx
@@ -11,10 +11,5 @@ function GlobalProvider({ children }) {
     </TasksContext.Provider>
   );
 }
-// Definiamo un hook per consumare il contesto
-// function useCount() {
-//   const context = useContext(CountContext);
-//   return context;
-// }
-// Esportiamo il nostro provider ed il nostro hook
+
 export { GlobalProvider, TasksContext };
